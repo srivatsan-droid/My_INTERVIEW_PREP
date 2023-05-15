191a5b32cdcffc883a57f658b3ee7ca8650ce379
Srivatsan <80402326+srivatsan-droid@users.noreply.github.com>
2023-05-15 23:35:12 +0530

    Add files via upload

---
+++ ProductOfTwoNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ProductOfTwoNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.println("Enter the Number: "+a);
+		int b = sc.nextInt();
+		System.out.println("Enter the Number2: "+b);
+		int product = a*b;
+		System.out.println("a" + "*" + "b" +  " " + product);
+	}
+
+}
